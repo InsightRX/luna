7f42abc0710686cb2f5f2ac49b64f6139d0149ad
Ron Keizer <ron@insight-rx.com>
2025-06-11 23:07:08 -0700

    misc package fixes

--- R/check_llm_key.R
+++ R/check_llm_key.R
@@ -3,7 +3,7 @@
 check_llm_key <- function() {
   api_key <- Sys.getenv("OPENAI_API_KEY")
   if(api_key == "") {
-    cli::cli_alert_info(
+    packageStartupMessage(
       paste(
         "The environment variable OPENAI_API_KEY is not set.",
         "Please set this to take full advantage of agentic support."
