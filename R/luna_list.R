0937188638c5a4c08a863f19175a5719219b7eb9
Ron Keizer <ron@insight-rx.com>
2025-06-27 06:10:46 -0700

    add ofv and dofv to luna_list()

--- R/luna_list.R
+++ R/luna_list.R
@@ -1,34 +1,48 @@
 #' List runs in current project
 #'
-#' @param project project name. Default is `NULL`, will use active project
+#' @param raw if `TRUE`, will return a tibble with all information
+#' this is more useful for exporting to a file rather than viewing
+#' in console.
 #'
 #' @export
 #'
 luna_list <- function(
-  project = NULL
+  raw = FALSE
 ) {
 
-  if(is.null(project)) {
-    project <- .luna_cache$get("project")
-  }
-
-  out <- project |>
-    runs_as_table()
-
+  ## Get cache and config
+  is_luna_cache_available(abort = TRUE)
   config <- get_luna_config()
-  cols <- config$general$list$columns
-  if(!is.null(cols)) {
-    sel_cols <- names(cols)[unlist(cols)]
-    check_unknown <- setdiff(sel_cols, names(out))
-    if(length(check_unknown) > 0) {
-      cli::cli_alert_warning("Columns {check_unknown} not available, please check configuration.")
-      sel_cols <- intersect(sel_cols, names(out))
-    }
-    if(length(sel_cols) > 0) {
-      out <- out |>
-        dplyr::select(!!sel_cols)
-    } else {
-      cli::cli_warning("No columns selected, please check luna configuration.")
+  name <- .luna_cache$get("project")$metadata$name
+  folder <- .luna_cache$get("project")$metadata$folder
+
+  ## make sure we're up to date
+  luna_load_project(
+    name = name,
+    folder = folder,
+    verbose = FALSE
+  )
+
+  ## Load table
+  project <- .luna_cache$get("project")
+  out <- runs_as_table(project, raw = raw)
+
+  ## select columns
+  if(!raw) {
+    cols <- config$general$list$columns
+    if(!is.null(cols)) {
+      sel_cols <- names(cols)[unlist(cols)]
+      check_unknown <- setdiff(sel_cols, names(out))
+      if(length(check_unknown) > 0) {
+        cli::cli_alert_warning("Columns {check_unknown} not available, please check configuration.")
+        sel_cols <- intersect(sel_cols, names(out))
+      }
+      if(length(sel_cols) > 0) {
+        out <- out |>
+          dplyr::select(!!sel_cols)
+      } else {
+        cli::cli_warning("No columns selected, please check luna configuration.")
+      }
     }
   }
 
