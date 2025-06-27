0722f310a1e00bdfde3b6e4cce8789265958e7b1
Ron Keizer <ron@insight-rx.com>
2025-06-27 03:58:33 -0700

    misc fixes

--- R/luna_list.R
+++ R/luna_list.R
@@ -1,15 +1,37 @@
 #' List runs in current project
 #'
+#' @param project project name. Default is `NULL`, will use active project
+#'
+#' @export
+#'
 luna_list <- function(
-  project = NULL,
-  return_object = FALSE
+  project = NULL
 ) {
 
   if(is.null(project)) {
     project <- .luna_cache$get("project")
   }
 
-  project |>
+  out <- project |>
     runs_as_table()
+
+  config <- get_luna_config()
+  cols <- config$general$list$columns
+  if(!is.null(cols)) {
+    sel_cols <- names(cols)[unlist(cols)]
+    check_unknown <- setdiff(sel_cols, names(out))
+    if(length(check_unknown) > 0) {
+      cli::cli_alert_warning("Columns {check_unknown} not available, please check configuration.")
+      sel_cols <- intersect(sel_cols, names(out))
+    }
+    if(length(sel_cols) > 0) {
+      out <- out |>
+        dplyr::select(!!sel_cols)
+    } else {
+      cli::cli_warning("No columns selected, please check luna configuration.")
+    }
+  }
+
+  out
 
 }
