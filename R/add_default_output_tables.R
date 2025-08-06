InsightRX/luna

commit 43721b0ad816721cc163188163c65befe8387ad1

fix pharmpy bug

R/add_default_output_tables.R
@@ -3,6 +3,8 @@
 #' if they don't already exist in the model.
 #'
 #' @param model Pharmpy model object
+#' @param iiv vector of parameters with iiv. Optional, if not specified
+#' will use pharmpy function to retrieve it.
 #' @param tables character vector of which default tables
 #' to add, options are `fit` and `parameters`.
 #' @param full_tables For the default tables, should all input columns from be
@@ -13,6 +15,7 @@
 #'
 add_default_output_tables <- function(
   model,
+  iiv = NULL,
   tables = c("fit", "parameters"),
   full_tables = FALSE,
   remove_existing = TRUE,
@@ -35,7 +38,12 @@ add_default_output_tables <- function(
   ## individual parameters, first row only
   if("parameters" %in% tables && !(default_table_names[["parameters"]] %in% existing_tables)) {
     if(verbose) cli::cli_alert_info("Adding output table for individual parameters")
-    cols <- pharmr::get_individual_parameters(model)
+    if(is.null(iiv)) {
+      ## Pharmpy bug, cannot retrieve IIV if only one parameter has IIV
+      cols <- pharmr::get_individual_parameters(model)
+    } else {
+      cols <- iiv
+    }
     if(full_tables) {
       cols <- unique(c(cols, model$datainfo$names))
     }

R/create_model.R
@@ -245,6 +245,7 @@ create_model <- function(
     if(!is.null(tables)) {
       mod <- add_default_output_tables(
         model = mod,
+        iiv = iiv,
         tables = tables,
         full_tables = full_tables
       )

man/add_default_output_tables.Rd
@@ -7,6 +7,7 @@ if they don't already exist in the model.}
 \usage{
 add_default_output_tables(
   model,
+  iiv = NULL,
   tables = c("fit", "parameters"),
   full_tables = FALSE,
   remove_existing = TRUE,
@@ -15,6 +16,9 @@ add_default_output_tables(
 }
 \arguments{
 \item{model}{Pharmpy model object}
+
+\item{iiv}{vector of parameters with iiv. Optional, if not specified
+will use pharmpy function to retrieve it.}
 
 \item{tables}{character vector of which default tables
 to add, options are \code{fit} and \code{parameters}.}

commit 66c309935a30db30e3786f2df0e0f24a9a04abb1

Fix $TABLE output when using iiv="basic" or iiv="all"
  When create_model() is called with iiv="basic" or iiv="all", these
  shortcut strings were being passed directly to add_default_output_tables()
  and used as column names, resulting in "$TABLE ID basic" instead of the
  actual parameter names like "$TABLE ID CL TVKA V".

  This fix checks if iiv is a shortcut string ("basic" or "all") and treats
  it the same as NULL, causing the function to auto-detect parameters using
  pharmr::get_individual_parameters() instead of using the string literally.

  The bug was introduced in commit 43721b0 which added the iiv parameter
  to work around a pharmpy issue with single-parameter IIV detection.

R/add_default_output_tables.R
@@ -38,8 +38,9 @@ add_default_output_tables <- function(
   ## individual parameters, first row only
   if("parameters" %in% tables && !(default_table_names[["parameters"]] %in% existing_tables)) {
     if(verbose) cli::cli_alert_info("Adding output table for individual parameters")
-    if(is.null(iiv)) {
+    if(is.null(iiv) || (is.character(iiv) && length(iiv) == 1 && iiv %in% c("basic", "all"))) {
       ## Pharmpy bug, cannot retrieve IIV if only one parameter has IIV
+      ## Also ignore shortcut strings like "basic" or "all"
       cols <- pharmr::get_individual_parameters(model)
     } else {
       cols <- iiv