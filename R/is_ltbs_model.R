a910ac87f3ca33594a539f789ab15c5f15bd9f34
Ron Keizer <ron@insight-rx.com>
2025-11-10 09:45:19 -0800

    carve out nlmixr2 check for ltbs

--- R/is_ltbs_model.R
+++ R/is_ltbs_model.R
@@ -5,6 +5,11 @@
 #' @export
 #'
 is_ltbs_model <- function(model) {
+  if(! "control_stream" %in% names(model$internals)) {
+    ## TODO: not yet implemented for nlmixr2!
+    cli::cli_alert_warning("Check for LTBS not yet implemented for nlmixr2.")
+    return(FALSE)
+  }
   res <- model$internals$control_stream$get_error_record()
   if(!is.null(res) && inherits(res, "pharmpy.model.external.nonmem.records.code_record.CodeRecord")) {
     y <- res$statements$find_assignment("Y")
