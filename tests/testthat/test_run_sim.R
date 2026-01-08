aa742ba20e1315e622278c92e16a62374100023b
Ron Keizer <ron@insight-rx.com>
2026-01-07 22:10:58 -0800

    add new test placeholder

---
+++ tests/testthat/test_run_sim.R
@@ -0,0 +1,9 @@
+## TODO: add tests for simulations
+## 
+## - basic simulations
+## - test case for this fix: https://github.com/InsightRX/luna/pull/56
+
+test_that(
+  "dummy check",
+  expect_true(TRUE)
+)
